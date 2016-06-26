7bfd68ff41401d6af065c8e1885270054f6e2280
Josh Rickard <josh.rickard@phishme.com>
2016-06-26 13:56:25 -0400

    Creating Module structure for future development

---
+++ Posh-NessusReporting.psm1
@@ -0,0 +1,26 @@
+﻿#requires -Version 2
+#Get public and private function definition files.
+$Public  = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
+$Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
+
+#Dot source the files
+Foreach($import in @($Public + $Private))
+{
+    Try
+    {
+        . $import.fullname
+    }
+    Catch
+    {
+        Write-Error -Message "Failed to import function $($import.fullname): $_"
+    }
+}
+
+#download and import POSH-VirusTotal
+if (!(Test-Path -Path "$Home\Documents\WindowsPowerShell\Modules\Posh-VirusTotal"))
+{
+    Write-Verbose -Message 'Downloading Posh-VirusTotal PowerShell Module....'
+    Invoke-Expression -Command (New-Object -TypeName Net.WebClient).DownloadString('https://gist.githubusercontent.com/darkoperator/9138373/raw/22fb97c07a21139a398c2a3d6ca7e3e710e476bc/PoshVTInstall.ps1')
+}
+
+Export-ModuleMember -Function $Public.Basename
